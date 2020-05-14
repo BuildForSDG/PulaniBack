a0cf8646cdf28a31dab96a225ac5f3ec43594e83
Anthony Nata <nataanthoni@gmail.com>
2020-05-14 00:33:24 -0700

    Restructured User and RequestLoad models

--- app/Loan.php
+++ app/Loan.php
@@ -9,25 +9,29 @@ class Loan extends Model
 {
     use SoftDeletes;
 
-    public $table = 'loans';
+    protected $table = 'loans';
+
+    public $primary_key = 'id';
+  
+    public $timestamp = 'true';
 
     protected $fillable = [
         'user_id',
-        'name_of_business',
-        'address_of_business',
+        'loanrequestamount',
+        'proposedcollaterals',
+        'purposeofloan',
+        'loanwithanotherbank',
+        'detailsofotherloan',
         'location_of_business',
-        'telephone_number',
-        'number_of_years_in_business',
-        'total_business_capital',
-        'loan_request_amount',
-        'proposed_collaterals',
-        'direction_of_route_to_business',
-        'direction_of_route_to_residence',
+        'loanperiod',
+        'interestrate',
+        'loanissuer',
         'date_form_submitted',
     ];
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this -> belongsTo('App\User');
+        //return $this->belongsTo(User::class, 'user_id');
     }
 }
